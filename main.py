c31c13dd5941b37ec2e677805876667aa82f2bb2
Fleege <76089935+pfleege@users.noreply.github.com>
2024-10-17 13:06:08 +0300

    ADD road image to game canvas

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ pygame.init()
 
 # Game canvas variables
 width = 800
-height = 600
+height = 700
 backgroundImg = pygame.image.load("background.jpg")
+roadImg = pygame.image.load("road.jpg")
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Flappy Clone")
 
 run = True
 while run:
+    screen.blit(backgroundImg, (0, 0))
+    screen.blit(roadImg, (0, 530))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    screen.blit(backgroundImg, (0, 0))
+
     pygame.display.flip()
 
 pygame.quit()
