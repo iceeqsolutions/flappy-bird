00923b23abc1233e13e68936731d47017be797cf
Fleege <76089935+pfleege@users.noreply.github.com>
2024-10-17 12:51:23 +0300

    ADD background image to game canvas

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from pygame.locals import *
 
 pygame.init()
 
+# Game canvas variables
 width = 800
 height = 600
+backgroundImg = pygame.image.load("background.jpg")
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Flappy Clone")
@@ -14,5 +16,7 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    screen.blit(backgroundImg, (0, 0))
+    pygame.display.flip()
 
 pygame.quit()
